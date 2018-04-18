e3e058f5d08728a718e3f4eb79446a08b8519764
8igMac <michaelshih123@gmail.com>
2018-04-18 19:20:08 +0800

    game GUI: finish board drawing

--- gomoku.py
+++ gomoku.py
@@ -8,7 +8,7 @@ class gomoku:
         self.frame.pack(fill='both', expand=True)
 
         # start a canvas
-        self.canvas = Canvas(self.frame, width=600, height=500)
+        self.canvas = Canvas(self.frame, width=320, height=340)
         self.canvas.pack(fill='both', expand=True)
 
         # create a label
@@ -29,7 +29,19 @@ class gomoku:
         self._board()
 
     def _board(self):
-        pass
+        for x in range(9):
+            self.canvas.create_line(80+x*20,0,x*10,160+x*20)
+            self.canvas.create_line(320-(80+x*20),320,320-x*10,160-x*20)
+
+        for y in range(9):
+            self.canvas.create_line(80+y*20,0,240+y*10,320-y*20)
+            self.canvas.create_line(80-y*10,0+y*20,240-y*20,320)
+
+        for z in range(9):
+            self.canvas.create_line(80-z*10,0+z*20,240+z*10,0+z*20)
+            self.canvas.create_line(80-z*10,320-z*20,240+z*10,320-z*20)
+
+        self.canvas.create_line(80,0,240,0)
 
     def startSg(self):
         pass
@@ -39,7 +51,7 @@ class gomoku:
         self.canvas.delete(ALL)
         self.label['text'] = "Gomoku game: double player mode"
         self.canvas.bind("<Button-1>", self.dbPlayer)
-        self.board()
+        self._board()
         # set up game state
         self.game_state = [0 for x in range(217)] 
         # set up game turns
